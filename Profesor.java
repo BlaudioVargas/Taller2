c3ef1a56535496b97ddf5c784fa94b49ddbdbc9d
BlaudioVargas <73751250+BlaudioVargas@users.noreply.github.com>
2021-11-29 16:24:14 -0300

    Update Profesor.java

--- Profesor.java
+++ Profesor.java
@@ -3,19 +3,37 @@ package Taller2;
 public class Profesor  extends Usuario{
 	private int salary;
 	
-	public Profesor(String rut, String email, String password, Paralelos paralel, int level) {
-		super(rut,email,password,paralel);
+	public Profesor(String rut, String email, String password, int level) {
+		super(rut,email,password);
 		this.salary=level;
 	}
-	@Override
+
 	public String getTipo() {
-		// TODO Auto-generated method stub
 		return "Profesor";
 	}
 
-	@Override
 	public int getInt() {
-		// TODO Auto-generated method stub
 		return this.salary;
+	}
+
+	public boolean addParalel(Paralelos paralel) {
+		int total = 0;
+		if(this.paralel==null) {
+			this.paralel= paralel;
+		}
+		else {
+			Paralelos indice = this.paralel;
+			while(indice.getNext()!=null) {
+				total ++;
+				indice=indice.getNext();
+			}
+			if(total<4) {
+				indice.setNext(paralel);
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
 	}
 }
