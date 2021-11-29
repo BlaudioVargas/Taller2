f021d932241e35d60df16a381529a3dcc10fda98
BlaudioVargas <73751250+BlaudioVargas@users.noreply.github.com>
2021-11-29 16:23:53 -0300

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -3,20 +3,38 @@ package Taller2;
 public class Estudiante extends Usuario{
 	private int level;
 	
-	public Estudiante(String rut, String email, String password, Paralelos paralel, int level) {
-		super(rut,email,password,paralel);
+	public Estudiante(String rut, String email, String password, int level) {
+		super(rut,email,password);
 		this.level=level;
 	}
-	@Override
+
 	public String getTipo() {
-		// TODO Auto-generated method stub
 		return "Estudiante";
 	}
 
-	@Override
 	public int getInt() {
-		// TODO Auto-generated method stub
 		return this.level;
+	}
+	
+	public boolean addParalel(Paralelos paralel){
+		int total = 0;
+		if(this.paralel==null) {
+			this.paralel= paralel;
+		}
+		else {
+			Paralelos indice = this.paralel;
+			while(indice.getNext()!=null) {
+				total += indice.getItemA().getItemA().getCredit();
+				indice=indice.getNext();
+			}
+			if((total+paralel.getItemA().getItemA().getCredit())<40) {
+				indice.setNext(paralel);
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
