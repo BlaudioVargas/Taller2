6a96ecccb8cc807095c79c28a5450832139baddd
BlaudioVargas <73751250+BlaudioVargas@users.noreply.github.com>
2021-11-29 16:21:08 -0300

    Update Asignatura.java

--- Asignatura.java
+++ Asignatura.java
@@ -5,8 +5,9 @@ public abstract class Asignatura {
 	private String name;
 	private int credit;
 	private int lvl;
-	private Paralelo paralel;
-	private Asignaturas prer;
+	protected Paralelo paralel;
+	@SuppressWarnings("unused")
+	protected Asignaturas prer;
 	
 	public Asignatura(String code, String name, int credit, int lvl) {
 		this.code=code;
@@ -19,9 +20,7 @@ public abstract class Asignatura {
 	
 	abstract public boolean addCoruse(Asignatura course);
 	
-	public boolean addParalel(Paralelos paralel) {
-		return true;
-	}
+	abstract public boolean addParalel(Paralelos paralel) ;
 	
 	public String getCode() {return this.code;}
 	public String getName() {return this.name;}
@@ -29,7 +28,7 @@ public abstract class Asignatura {
 	abstract public String getType() ;
 	public int getLevel() {return this.lvl;}
 	
-	abstract public Paralelo getParalel() ;
+	public Paralelo getParalel() {return this.paralel;} 
 	abstract public Asignaturas getPrerequisits() ;
 
 }
