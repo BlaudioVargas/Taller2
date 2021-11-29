5f38e28278db7666b408907ae0316affa2598a05
BlaudioVargas <73751250+BlaudioVargas@users.noreply.github.com>
2021-11-29 16:23:25 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -4,14 +4,16 @@ public abstract class Usuario {
 	private String rut;
 	private String email;
 	private String password;
-	private Paralelos paralel;
+	protected Paralelos paralel;
 	
-	public Usuario (String rut, String email, String password, Paralelos paralel) {
+	public Usuario (String rut, String email, String password) {
 		this.rut=rut;
 		this.email=email;
 		this.password=password;
-		this.paralel=paralel;
+		this.paralel=null;
 	}
+	
+	abstract public boolean addParalel(Paralelos paralel);
 	
 	public String getRut(){return this.rut;}
 	public String getEmail(){return this.email;}
