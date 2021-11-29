5d9d5049d5ee394963802f65e3ab74aa4f6309de
BlaudioVargas <73751250+BlaudioVargas@users.noreply.github.com>
2021-11-29 16:21:56 -0300

    Update AsignaturaO.java

--- AsignaturaO.java
+++ AsignaturaO.java
@@ -5,28 +5,46 @@ public class AsignaturaO extends Asignatura{
 	public AsignaturaO(String code, String name, int credit, int lvl) {
 		super(code,name,credit,lvl);
 	}
-	@Override
+	
 	public boolean addCoruse(Asignatura course) {
-		// TODO Auto-generated method stub
-		return false;
+		if(this.prer==null) {
+			this.prer= new Asignaturas(course);
+		}
+		else {
+			Asignaturas indice = this.prer;
+			while(indice.getNext()!=null) {
+				indice=indice.getNext();
+			}
+			indice.setNext(new Asignaturas(course));
+		}
+		return true;
 	}
 
-	@Override
+	
 	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return "obligatoria";
 	}
 
-	@Override
-	public Paralelo getParalel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
+	
 	public Asignaturas getPrerequisits() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.prer;
+	}
+	
+	public boolean addParalel(Paralelos paralel) {
+		int contador =1;
+		if(this.paralel==null) {
+			this.paralel= new Paralelo(this, paralel, contador);
+		}
+		else {
+			Paralelo indice = this.paralel;
+			contador++;
+			while(indice.getNext()!=null) {
+				indice=indice.getNext();
+				contador++;
+			}
+			indice.setNext(new Paralelo(this, paralel, contador));
+		}
+		return false;
 	}
 
 }
